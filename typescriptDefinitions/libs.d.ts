03366641519ac08e6a4929d6ea48634bd0f28da2
Paul Bakker <paul.bakker@luminis.eu>
2015-08-10 13:57:16 +0200

    moved rx typescript definitions

--- typescriptDefinitions/libs.d.ts
+++ typescriptDefinitions/libs.d.ts
@@ -1,7 +1,7 @@
 /// <reference path="./angularjs/angular.d.ts" />
 /// <reference path="./angularjs/angular-route.d.ts" />
 /// <reference path="./angular-bootstrap/angular-ui-bootstrap.d.ts" />
-/// <reference path="../bower_components/rxjs/ts/rx.d.ts"/>
+/// <reference path="./rx/rx.d.ts"/>
 /// <reference path="./model/Member.d.ts"/>
 /// <reference path="./lodash/lodash.d.ts"/>
 
